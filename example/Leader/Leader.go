cd9f8045cc085c9b9f8c2d86897a2dad238d326a
arieswang <x8853191@gmail.com>
2023-07-21 17:03:09 +0800

    增加範例

---
+++ example/Leader/Leader.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/wangyuche/goutils/log"
+	"github.com/wangyuche/workercluster"
+	"github.com/wangyuche/workercluster/core/worker"
+)
+
+func main() {
+	log.New(log.LogType(os.Getenv("LogType")))
+	w := workercluster.New()
+	w.Run("aaa", "default", os.Getenv("HOSTNAME"), "8888", &WorkerServerCallBack{})
+	ch := make(chan os.Signal, 1)
+	<-ch
+}
+
+type WorkerServerCallBack struct {
+}
+
+func (this *WorkerServerCallBack) WorkerRecv(data []byte) {
+
+}
+
+func (this *WorkerServerCallBack) WorkerIdleEvent(server *worker.WorkerInfo) {
+
+}
+
+func (this *WorkerServerCallBack) MasterRecv(data []byte) {
+
+}
+
+func (this *WorkerServerCallBack) BeMaster() {
+}
